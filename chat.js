bda2e4bfc96f97ca6dd15add4612937ac8a672f9
paulmaloba <163733267+paulmaloba@users.noreply.github.com>
2024-12-03 04:38:02 -0800

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,11 @@
+const toChatBtn = document.querySelector(".to-chat");
+const noChatMsg = document.querySelector("#no-group-chat");
+ toChatBtn.addEventListener('click', () => {
+    document.querySelector(".no-group-chat").style.display = "block";
+    
+    toChatBtn.textContent = "Subscribe";
+    toChatBtn.addEventListener('click', () => {
+        const supMessage = "https://wa.me/260771561954?text=Greetings%20Paul,I'd%20like%20to%20subscribe%20for%20Unistudy%20Connect";
+        window.open(supMessage, '_blank');
+    });
+ });
