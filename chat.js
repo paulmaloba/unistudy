d8c32ee5caa1f3d42fb4677ae1c82490c972e1b5
paulmaloba <163733267+paulmaloba@users.noreply.github.com>
2024-12-03 05:19:35 -0800

    Update chat.js

--- chat.js
+++ chat.js
@@ -3,7 +3,7 @@ const noChatMsg = document.querySelector("#no-group-chat");
  toChatBtn.addEventListener('click', () => {
     document.querySelector(".no-group-chat").style.display = "block";
     
-    toChatBtn.textContent = "Subscribe";
+    toChatBtn.style.dispaly = "none";
     toChatBtn.addEventListener('click', () => {
         const supMessage = "https://wa.me/260771561954?text=Greetings%20Paul,I'd%20like%20to%20subscribe%20for%20Unistudy%20Connect";
         window.open(supMessage, '_blank');
